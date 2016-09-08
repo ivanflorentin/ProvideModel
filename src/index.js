aced89a6cde7082c56571f579dd8e2cea47a82b3
Ivan Florentin <ivan@sinergetica.com>
2016-09-07 21:35:51 -0400

    renamed to model

--- src/index.js
+++ src/index.js
@@ -1,93 +1,93 @@
 import uuid from 'node-uuid'
 
-export default function (compDef) {
-  const {componentName,
+export default function (modelDef) {
+  const {modelName,
        fields,
        listName,
        replication
-	} = compDef
+	} = modelDef
 
   const listProperName = listName[0].toUpperCase() + listName.substring(1)
-  const componentListName = componentName + 'List'
+  const modelListName = modelName + 'List'
   const listCapitalName = listName.toUpperCase()
-  const componentProperName = componentName[0].toUpperCase() + componentName.substring(1)
-  const componentCapitalName = componentName.toUpperCase()
-  const routesName = componentName + 'Routes'
-  const templatesName = componentName + 'Templates'
-  const componentDefinitionName = componentName + 'ComponentDef'
+  const modelProperName = modelName[0].toUpperCase() + modelName.substring(1)
+  const modelCapitalName = modelName.toUpperCase()
+  const routesName = modelName + 'Routes'
+  const templatesName = modelName + 'Templates'
+  const modelDefinitionName = modelName + 'ModelDef'
 
   let fieldNames = Object.keys(fields)
-  const SELECT = `SELECT_${componentCapitalName}`
-  const DESELECT = `DESELECT_${componentCapitalName}`
-  const SAVE = `SAVE_${componentCapitalName}`
+  const SELECT = `SELECT_${modelCapitalName}`
+  const DESELECT = `DESELECT_${modelCapitalName}`
+  const SAVE = `SAVE_${modelCapitalName}`
   const LOAD = `LOAD_${listCapitalName}`
-  const STORE = `STORE_${componentCapitalName}`
-  const DELETE = `DELETE_${componentCapitalName}`
-  const SET_ROUTE = `SET_ROUTE_${componentCapitalName}`
-  const DELETE_ROUTE = `DELETE_ROUTE_${componentCapitalName}`
-  const SET_TEMPLATE = `SET_TEMPLATE_${componentCapitalName}`
-  const DELETE_TEMPLATE = `DELETE_TEMPLATE_${componentCapitalName}`
-  const SET_COMPONENT_DEF = `SET_COMPOENET_DEF_${componentCapitalName}`
+  const STORE = `STORE_${modelCapitalName}`
+  const DELETE = `DELETE_${modelCapitalName}`
+  const SET_ROUTE = `SET_ROUTE_${modelCapitalName}`
+  const DELETE_ROUTE = `DELETE_ROUTE_${modelCapitalName}`
+  const SET_TEMPLATE = `SET_TEMPLATE_${modelCapitalName}`
+  const DELETE_TEMPLATE = `DELETE_TEMPLATE_${modelCapitalName}`
+  const SET_MODEL_DEF = `SET_COMPOENET_DEF_${modelCapitalName}`
   const SAVE_RELATION = `SAVE_RELATION`
   const DELETE_RELATION = `DELETE_RELATION`
 
   const actions = {}
   const reducers = {}
 
-  actions[`select${componentProperName}`] = (component) =>{
-    return {type: SELECT, component}
+  actions[`select${modelProperName}`] = (model) =>{
+    return {type: SELECT, model}
   }
-  actions[`deselect${componentProperName}`] = () =>{
+  actions[`deselect${modelProperName}`] = () =>{
     return {type: DESELECT}
   }
-  actions[`save${componentProperName}`] = (component) =>{
-    return {type: SAVE, component}
+  actions[`save${modelProperName}`] = (model) =>{
+    return {type: SAVE, model}
   }
   actions[`load${listProperName}`] = (list) =>{
     return {type: LOAD, list}
   }
-  actions[`store${componentProperName}`] = (component) =>{
-    return {type: STORE, component}
+  actions[`store${modelProperName}`] = (model) =>{
+    return {type: STORE, model}
   }
-  actions[`delete${componentProperName}`] = (component) =>{
-    return {type: DELETE, component}
+  actions[`delete${modelProperName}`] = (model) =>{
+    return {type: DELETE, model}
   }
-  actions[`setRoute${componentProperName}`] = (routeDef) =>{
+  actions[`setRoute${modelProperName}`] = (routeDef) =>{
     return {type: SET_ROUTE, ...routeDef}
   }
-  actions[`deleteRoute${componentProperName}`] = (routeDef) =>{
+  actions[`deleteRoute${modelProperName}`] = (routeDef) =>{
     return {type: DELETE_ROUTE, ...routeDef}
   }
-  actions[`setTemplate${componentProperName}`] = (templateDef) =>{
+  actions[`setTemplate${modelProperName}`] = (templateDef) =>{
     return {type: SET_TEMPLATE, ...templateDef}
   }
-  actions[`deleteTemplate${componentProperName}`] = (templateDef) =>{
+  actions[`deleteTemplate${modelProperName}`] = (templateDef) =>{
     return {type: DELETE_TEMPLATE, ...templateDef}
   }
-  actions[`setComponentDef${componentProperName}`] = (componentDef) =>{
-    return {type: SET_COMPONENT_DEF, componentDef}
+  actions[`setModelDef${modelProperName}`] = (def) =>{
+    return {type: SET_MODEL_DEF, modelDef: def}
   }
 
   if (fieldNames.length > 0) {
     for (let fieldName of fieldNames) {
       const fieldDef = fields[fieldName]
       const relationName = fieldDef.relation
-      if (relationName !== undefined) {
+      if (relationName) {
 	const relationProperName = relationName[0].toUpperCase() + relationName.substring(1)
-	actions[`save${componentProperName}${relationProperName}`] = (id) =>{
+	actions[`save${modelProperName}${relationProperName}`] = (id) =>{
 	  return {type: SAVE_RELATION, fieldName, id}
 	}
-	actions[`delete${componentProperName}${relationProperName}`] = (id) =>{
+	actions[`delete${modelProperName}${relationProperName}`] = (id) =>{
 	  return {type: DELETE_RELATION, fieldName, id}
 	}
       }
     }
   }
 
-  reducers[componentDefinitionName] = (state = {}, action) =>{
+  reducers[modelDefinitionName] = (state = {}, action) =>{
     switch (action.type) {
-    case SET_COMPONENT_DEF: {
-      return action.componentDef
+    case SET_MODEL_DEF: {
+      return action.modelDef
     }
     default: return state
     }
@@ -129,19 +129,19 @@ export default function (compDef) {
     }
   }
 
-  reducers[componentName] = (state = {isValid: false}, action) =>{
+  reducers[modelName] = (state = {isValid: false}, action) =>{
     switch (action.type) {
     case SELECT: {
-      return action.component
+      return action.model
     }
     case DESELECT: {
       return {}
     }
     case SAVE: {
-      const next = JSON.parse(JSON.stringify(action.component))
+      const next = JSON.parse(JSON.stringify(action.model))
       next.isValid = true
       for (let fieldName of fieldNames) {
-	const value = String(action.component[fieldName])
+	const value = String(action.model[fieldName])
 	const validators = fields[fieldName].validate
 	if (validators && validators.length > 0) {
 	  next.isValid= true
@@ -162,12 +162,12 @@ export default function (compDef) {
     }
     case SAVE_RELATION: {
       const {fieldName, id} = action
-      if (id === undefined) {
+      if (!id) {
 	return state
       }
       const next = Object.assign({}, state)
       next[fieldName] = Object.assign({}, state[fieldName])
-      if (next[fieldName] === undefined) {
+      if (!next[fieldName]) {
 	next[fieldName] = {}
       }
       next[fieldName][id] = id
@@ -184,7 +184,7 @@ export default function (compDef) {
     }
   }
 
-  reducers[componentListName] = (state = {}, action) =>{
+  reducers[modelListName] = (state = {}, action) =>{
     switch (action.type) {
     case LOAD: {
       return action.list
@@ -192,16 +192,16 @@ export default function (compDef) {
     case STORE: {
        let id = ''
       const next = {...state}
-      if (action.component.uuid) {
-	id = action.component.uuid
-	next[id] = Object.assign({}, action.component, {uuid: id})
+      if (action.model.uuid) {
+	id = action.model.uuid
+	next[id] = Object.assign({}, action.model, {uuid: id})
       } else { id = uuid.v4() }
-      next[id] = Object.assign({}, action.component, {uuid: id})
+      next[id] = Object.assign({}, action.model, {uuid: id})
       return next
     }
     case DELETE: {
       const next = Object.assign({}, state)
-      delete next[action.component.uuid]
+      delete next[action.model.uuid]
       return next
     }
     default: {
